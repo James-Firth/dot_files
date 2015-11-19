8af522434ae3b2ee5ab6a6cee89d6462d08352c4
James Firth <hello@james-firth.com>
2015-11-18 22:39:58 -0600

    Added initial setup script.

---
+++ setup_ubuntu.sh
@@ -0,0 +1,7 @@
+# Ubuntu setup steps
+
+sudo apt-get update;
+sudo apt-get install -y git git-gui gitk vim terminator zsh i3;
+
+#install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
