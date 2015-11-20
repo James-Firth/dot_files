e2806fcadc64aa70d46e003b03ef4f31bab3503a
James-Firth <hello@james-firth.com>
2015-11-20 01:57:10 -0600

    Added some new config files. Include fonts and icons directly in project instead of linking to them.
    Added gitignore
    
    TODO: update readme with contribution notices to the owners.
    
    Thanks to FontAwesome, the creator of the Arc GTK Theme, and the Flattr icon creators.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dot_files
+
+### Requirements
+
+* Wallpaper file should be located at ~/Pictures/wallpaper.jpg
+
+## Setup
+
+* Run lxappearance and change themes/fonts
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,7 +1,43 @@
 # Ubuntu setup steps
 
+
+#CONFIG FILES and whatnot
+# .i3/config
+# ~/.fonts
+# ~/.gtkrc-2.0
+# ~/.config/gtk-3.0/settings.ini
+
+#Add this ppa:
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/arc-theme.list";
+sudo apt-get clean; #Needed to re-build the cache. otherwise the opensuse stuff freaks out.
 sudo apt-get update;
-sudo apt-get install -y git git-gui gitk vim terminator zsh i3;
+
+# Install apps
+sudo apt-get install -y git git-gui gitk vim terminator zsh i3 tmux lxappearance arc-theme rofi compton i3blocks thunar gnome-icon-theme-full;
+# Thunar and gnome-icon-theme-full are only requied if nautilus isn't working.
+
+#Install icon theme
+#firefox https://github.com/NitruxSA/flattr-icons/release
+s
+# move Flattr and Flattr Dark to /usr/share/icons/
 
 #install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Download Font-awesome
+#firefox https://github.com/FortAwesome/Font-Awesome/releases
+
+# Download system font (Yosemite San Francisco) and copy to ~/.fonts/
+#firefox https://github.com/FortAwesome/Font-Awesome/releases
+
+# Install Firefox theme
+#firefox https://github.com/horst3180/arc-firefox-theme/releases
+
+# Copy files to their appropriate locations.
+tar -xzf icon_themes.tar.gz -C /usr/share/icons/
+mv .fonts/ ~/
+mv .i3/ ~/
+mv .ssh/ ~/
+mv .tmux.conf/ ~/
+mv .vimrc ~/
+mv .xlock/ ~/
