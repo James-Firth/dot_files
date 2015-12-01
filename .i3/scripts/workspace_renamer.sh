cca4ecc21641534615e1c94a9dae1ddae28fb6c3
James Firth <hello@james-firth.com>
2015-12-01 09:41:56 -0600

    Added workspace renamer

---
+++ .i3/scripts/workspace_renamer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+#Thanks to this guy: https://github.com/deiwin/i3-dotfiles/blob/docs/.i3/rename_workspace.sh
+
+num=`i3-msg -t get_workspaces | jq 'map(select(.focused == true))[0].num'`
+i3-input -F "rename workspace to \"$num: %s\"" -P "New name $num: "
+
+name=`i3-msg -t get_workspaces | jq 'map(select(.focused == true))[0].name'`
+# If empty name was set
+if [[ "$name" =~ ^\"[0-9]+:\"$ ]]
+then
+  # Remove trailing colon and whitespace
+  i3-msg "rename workspace to \"$num\""
+fi
