9672b77a49bf490d75fe6a47527558d3b338bc82
James Firth <james.firth@norimaconsulting.com>
2017-01-10 13:43:57 -0600

    Updated configs. More icons! Better screenlock (for my setup)

--- .i3/scripts/screen_locker.sh
+++ .i3/scripts/screen_locker.sh
@@ -5,8 +5,16 @@ tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
 
+#Take screenshot
 scrot "$tmpbg"
+#Scale it to make it blur?
 convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-#i3lock -u -i "$tmpbg" -p win
+
+#overlay the icons
+convert "$tmpbg" "$icon" -gravity West -composite -matte "$tmpbg"
+convert "$tmpbg" "$icon" -gravity East -composite -matte "$tmpbg"
+
+# Old style
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+
 i3lock  -i "$tmpbg" -p win -f
