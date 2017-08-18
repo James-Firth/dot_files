b3c04046d9004e87d59837f2022828e51ddeb786
James Firth <james.firth@norimaconsulting.com>
2017-08-18 14:29:31 -0500

    General  Updates to i3 and dotfiles
    Added kb layouts to docks
    
    new screenlocker
    
    extra configs and auto starts

--- .i3/scripts/screen_locker.sh
+++ .i3/scripts/screen_locker.sh
@@ -9,12 +9,14 @@ tmpbg='/tmp/screen.png'
 scrot "$tmpbg"
 #Scale it to make it blur?
 convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
+convert "$tmpbg" -swirl 90 "$tmpbg"
+# convert "$tmpbg" -sketch 2 "$tmpbg"
 
 #overlay the icons
-convert "$tmpbg" "$icon" -gravity West -composite -matte "$tmpbg"
-convert "$tmpbg" "$icon" -gravity East -composite -matte "$tmpbg"
+# convert "$tmpbg" "$icon" -gravity West -composite -matte "$tmpbg"
+# convert "$tmpbg" "$icon" -gravity East -composite -matte "$tmpbg"
 
 # Old style
-#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
 i3lock  -i "$tmpbg" -p win -f
