99c7247c8897f75a547573574ce521bf15e1138f
James Firth <james.firth@norimaconsulting.com>
2017-09-21 12:01:13 -0500

    * Added scratchpad support with an extra youtube stickied * Added lockscreen image instead of the pixelated screenshots   * bonus is it locks much faster.

--- .i3/scripts/screen_locker.sh
+++ .i3/scripts/screen_locker.sh
@@ -6,19 +6,19 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 #Take screenshot
-scrot "$tmpbg"
+#scrot "$tmpbg"
 #Scale it to make it blur?
-convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
-convert "$tmpbg" -swirl 90 "$tmpbg"
-# convert "$tmpbg" -sketch 2 "$tmpbg"
+#convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
+#convert "$tmpbg" -swirl 90 "$tmpbg"
+## convert "$tmpbg" -sketch 2 "$tmpbg"
 
 #overlay the icons
-# convert "$tmpbg" "$icon" -gravity West -composite -matte "$tmpbg"
-# convert "$tmpbg" "$icon" -gravity East -composite -matte "$tmpbg"
+## convert "$tmpbg" "$icon" -gravity West -composite -matte "$tmpbg"
+## convert "$tmpbg" "$icon" -gravity East -composite -matte "$tmpbg"
 
 # Old style
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
 pkill -u "$USER" -USR1 dunst
-i3lock -n -i "$tmpbg" -p win -f --ignore-empty-password
+i3lock -n -i "$HOME/Pictures/wallpapers/lockscreen.png" -p win -f --ignore-empty-password
 pkill -u "$USER" -USR2 dunst
