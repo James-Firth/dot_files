4ff125063e93d375aa2aa5c6aa74c005f0a7ad04
James Firth <james.firth@norimaconsulting.com>
2019-09-16 11:47:50 -0500

    Add all untracked screen layouts

---
+++ .i3/scripts/screenlayout/home_2_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --primary --mode 1680x1050 --pos 0x72 --rotate normal --output eDP-1 --mode 1600x900 --pos 1680x150 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --mode 1920x1200 --pos 3280x0 --rotate normal
---
+++ .i3/scripts/screenlayout/triple_monitor_double_portrait.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output DP-2-1 --mode 1920x1080 --pos 0x176 --rotate normal --output DP-2-2 --primary --mode 1920x1080 --pos 1920x0 --rotate right --output DP-2-3 --mode 1920x1080 --pos 3000x0 --rotate right --output eDP-1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
