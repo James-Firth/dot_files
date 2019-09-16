92e51eb73b712d79672c99376706b515b29ba86d
James Firth <james.firth@norimaconsulting.com>
2019-09-16 11:46:16 -0500

    Update i3 configs, add triple monitor default for work. Update i3lock

--- .i3/scripts/screen_locker.sh
+++ .i3/scripts/screen_locker.sh
@@ -6,7 +6,7 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 #Take screenshot
-#scrot "$tmpbg"
+# scrot "$tmpbg"
 #Scale it to make it blur?
 #convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
 #convert "$tmpbg" -swirl 90 "$tmpbg"
@@ -21,6 +21,17 @@ tmpbg='/tmp/screen.png'
 
 pkill -u "$USER" -USR1 dunst
 # REMINDER: Lockscreen image MUST be a png not jpg
-# i3lock --nofork --image="$HOME/Pictures/wallpapers/lockscreen.png" --tiling --pointer=win --show-failed-attempts --ignore-empty-password
-i3lock --nofork -c aaaaaa --tiling --pointer=win --show-failed-attempts --ignore-empty-password
+
+# i3lock --nofork --image="$HOME/Pictures/wallpapers/lockscreen.png" -c 000000 -tiling --pointer=win --show-failed-attempts --ignore-empty-password;
+# i3lock --nofork -c aaaaaa --tiling --pointer=win --show-failed-attempts --ignore-empty-password
+
+# i3lock notes:
+# -t tiles
+
+#Use screenshot
+# i3lock -n -i "$tmpbg" -p win -f --ignore-empty-password
+
+# Use tiling background
+i3lock -n -i "/home/james/Pictures/wallpapers/tiling/what-the-hex-dark.png" -t -p win -f --ignore-empty-password
+# pattern from https://www.toptal.com/designers/subtlepatterns/what-the-hex-dark/
 pkill -u "$USER" -USR2 dunst
