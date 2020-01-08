99d9e56ec2b645bde741e39942143f9a39fa54fb
James Firth <james.firth@norimaconsulting.com>
2020-01-08 14:53:40 -0600

    Screen settings and vscode settings

--- .i3/scripts/screenlayout/home_2_monitors.sh
+++ .i3/scripts/screenlayout/home_2_monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --primary --mode 1680x1050 --pos 0x72 --rotate normal --output eDP-1 --mode 1600x900 --pos 1680x150 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --mode 1920x1200 --pos 3280x0 --rotate normal
+xrandr --output VGA-1 --mode 1680x1050 --pos 3520x136 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x136 --rotate normal --output HDMI-2 --off --output HDMI-1 --primary --mode 1920x1200 --pos 1600x0 --rotate normal --output DP-2 --off --output DP-1 --off
