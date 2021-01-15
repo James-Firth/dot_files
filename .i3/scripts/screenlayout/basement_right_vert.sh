14a495854fd8757aac088b7625e7ba337a3f4ef6
James Firth <james.firth@norimaconsulting.com>
2021-01-15 11:34:32 -0600

    Some i3 updates while working fro home

---
+++ .i3/scripts/screenlayout/basement_right_vert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output DP-2-1 --mode 1920x1080 --pos 3520x0 --rotate right --output DP-2-2 --primary --mode 1920x1080 --pos 1600x360 --rotate normal --output DP-2-3 --off --output eDP-1 --mode 1600x900 --pos 0x600 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
--- .i3/scripts/screenlayout/home_1_monitor.sh
+++ .i3/scripts/screenlayout/home_1_monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --primary --mode 1680x1050 --pos 1600x0 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x708 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+xrandr --output VGA-1 --off --output DP-2-1 --mode 1920x1200 --pos 1600x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --primary --mode 1600x900 --pos 3520x128 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
