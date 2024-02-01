634ab6796c9824be52cc301ac680b225e109f7a2
James Firth <james@feedflo.com>
2024-01-31 18:19:28 -0600

    Remove i3 configs, no longer using it. Can always grab from git

--- .i3/scripts/screenlayout/home_1_monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-1 --off --output DP-2-1 --mode 1920x1200 --pos 1600x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --primary --mode 1600x900 --pos 3520x128 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
